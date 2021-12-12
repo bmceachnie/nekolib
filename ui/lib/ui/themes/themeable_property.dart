bdccfcc0b53212cbbf6a69497fca94eb7f6ed199
BuildTools <bmceachnie@student.tgm.ac.at>
2021-12-12 16:09:28 +0100

    Update themeable_property.dart

--- ui/lib/ui/themes/themeable_property.dart
+++ ui/lib/ui/themes/themeable_property.dart
@@ -1,10 +1,14 @@
-import 'package:nekolib.ui/ui/themes/theme.dart';
 import 'package:nekolib.ui/ui/themes/themes.dart';
 
 class ThemeableProperty<T> {
   ThemeableProperty(Map<String, T> values) {
     assert(values.keys.toSet().containsAll(NcThemes.all.keys.toSet()));
     this.values = values;
+  }
+
+  ThemeableProperty.fromTheme(Map<NcTheme, T> values) {
+    assert(values.keys.toSet().containsAll(NcThemes.all.values.toSet()));
+    this.values = values.map((theme, value) => MapEntry(theme.name, value));
   }
 
   late final Map<String, T> values;
