f7358ce6f4ef3aa5b12aaa3001f3003fb5ec9716
BuildTools <bmceachnie@student.tgm.ac.at>
2022-03-08 22:47:02 +0100

    Update builder.dart

--- ui/lib/animations/builder.dart
+++ ui/lib/animations/builder.dart
@@ -1,7 +1,7 @@
 part of animations;
 
 /// A builder to adapt to the widget being hoverd or not.
-class HoverBuilder extends _HoverableWidget {
+class HoverBuilder extends HoverableWidget {
   const HoverBuilder({Key? key, required this.builder, MouseCursor cursor = kDefaultHoverCursor}) : super(key: key, cursor: cursor);
 
   /// The builder to build the widget based if the user [isHovering] or not.
