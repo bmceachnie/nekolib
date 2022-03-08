b2ea3ce0015ee67d7efa55fbbb4c8a9c545eadac
BuildTools <bmceachnie@student.tgm.ac.at>
2022-03-08 23:01:33 +0100

    tranform + offset on hover

--- ui/lib/animations.dart
+++ ui/lib/animations.dart
@@ -5,3 +5,5 @@ import 'package:flutter/material.dart';
 part 'animations/hoverable_widget.dart';
 part 'animations/scale.dart';
 part 'animations/builder.dart';
+part 'animations/transform.dart';
+part 'animations/offset.dart';
