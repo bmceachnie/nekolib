d0ed0dd63635991f78e44d030e270a762c6fda5f
bmceachnie <bmceachnie@student.tgm.ac.at>
2022-03-20 19:00:37 +0100

    Update color.dart

--- ui/lib/utils/extensions/color.dart
+++ ui/lib/utils/extensions/color.dart
@@ -4,7 +4,7 @@ part of utils;
 extension ColorUtils on Color {
   /// Returns a new [Color] darkened by the given [amount].
   /// Credit to: [NearHuscarl](https://stackoverflow.com/questions/58360989/programmatically-lighten-or-darken-a-hex-color-in-dart#:~:text=For%20people%20who%20want%20to%20darken%20or%20lighten%20Color%20instead%20of%20hex%20string)
-  Color darken({double amount = .1}) {
+  Color darken([double amount = .1]) {
     assert(amount >= 0 && amount <= 1);
 
     final hsl = HSLColor.fromColor(this);
@@ -15,7 +15,7 @@ extension ColorUtils on Color {
 
   /// Returns a new [Color] lightened by the given [amount].
   /// Credit to: [NearHuscarl](https://stackoverflow.com/questions/58360989/programmatically-lighten-or-darken-a-hex-color-in-dart#:~:text=For%20people%20who%20want%20to%20darken%20or%20lighten%20Color%20instead%20of%20hex%20string)
-  Color lighten({double amount = .1}) {
+  Color lighten([double amount = .1]) {
     assert(amount >= 0 && amount <= 1);
 
     final hsl = HSLColor.fromColor(this);
